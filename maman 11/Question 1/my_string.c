ec1456524fbbec8e2b7ccc969f5c99b1c0391cb0
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2023-03-29 19:27:23 +0300

    Update my_string.c

--- maman 11/Question 1/my_string.c
+++ maman 11/Question 1/my_string.c
@@ -72,36 +72,36 @@ int main() {
 
 
 int strcmp(const char* str1, const char* str2) {
-    // Iterate through both strings until a difference is found or the end of a string is reached
+    /* Iterate through both strings until a difference is found or the end of a string is reached */
     while (*str1 && (*str1 == *str2)) {
         str1++;
         str2++;
     }
-    // Return the difference of the ASCII values of the characters at the point where the strings differ
+    /* Return the difference of the ASCII values of the characters at the point where the strings differ */
     return *(const unsigned char*)str1 - *(const unsigned char*)str2;
 }
 
 int strncmp(const char* str1, const char* str2, size_t n) {
-    // Iterate through both strings up to n characters or until a difference is found or the end of a string is reached
+    /* Iterate through both strings up to n characters or until a difference is found or the end of a string is reached */
     for (size_t i = 0; i < n && (*str1 || *str2); i++, str1++, str2++) {
         if (*str1 != *str2) {
-            // Return the difference of the ASCII values of the characters at the point where the strings differ
+            /* Return the difference of the ASCII values of the characters at the point where the strings differ */
             return *(const unsigned char*)str1 - *(const unsigned char*)str2;
         }
     }
-    // If no differences are found in the first n characters, return 0
+    /* If no differences are found in the first n characters, return 0 */
     return 0;
 }
 
 char* strchr(const char* str, int ch) {
-    // Iterate through the string until the specified character is found or the end of the string is reached
+    /* Iterate through the string until the specified character is found or the end of the string is reached */
     while (*str != '\0') {
         if (*str == ch) {
-            // Return a pointer to the position where the specified character is found
+            /* Return a pointer to the position where the specified character is found */
             return (char*) str;
         }
         str++;
     }
-    // If the specified character is not found, return NULL
+    /* If the specified character is not found, return NULL */
     return NULL;
 }
