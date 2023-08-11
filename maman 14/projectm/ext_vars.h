178e4a02c6c6f263ec65871ff2df9b081b6008cc
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-08-11 14:16:54 +0300

    שדג

---
+++ maman 14/projectm/ext_vars.h
@@ -0,0 +1,18 @@
+/*****************************************
+	Michael Michael, Michael Michael
+*****************************************/
+
+#include "structs.h"
+#include "assembler.h"
+
+#define MACHINE_RAM 1024
+
+extern int ic, dc;
+extern int err;
+extern boolean was_error;
+extern labelPtr symbols_table;
+extern extPtr ext_list;
+extern const char base64[64];
+extern const char *commands[];
+extern const char *directives[];
+extern boolean entry_exists, extern_exists;
