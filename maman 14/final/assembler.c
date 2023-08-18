1ba296b3af12eed0382eae5d20e02f896aa90778
Your Name <you@example.com>
2023-08-18 08:09:40 +0000

    1

--- maman 14/final/assembler.c
+++ maman 14/final/assembler.c
@@ -8,46 +8,53 @@
 #include "symbol_table.h"
 #include "prints.h"
 
-/* assembler gets file names through terminal and proccess them one at a time, assuming file name given with no extension */
-int main (int argc, char* argv[])
-{
+/* Main function that reads filenames from the command line arguments
+   and processes each file separately. Filenames are assumed to be provided without extensions. */
+int main(int argc, char* argv[]) {
     int i;
-    char* fileName; /* the file name as given as argument */
+    char* fileName; /* Filename passed as a command-line argument */
     bool pre_proccesor, first_pass, second_pass;
 
-	if (argc < 2)
-	{
-		printf("Usage: %s <filename> <filename> ...\n", argv[0]);
-	}
-	 /* Processing each file name argument */
-    
-    for (i = 1; i < argc; i++) 
-    {   
+    if (argc < 2) {
+        printf("Usage: %s <filename> <filename> ...\n", argv[0]);
+        return 1; /* Return an error code if not enough arguments are provided */
+    }
+
+    /* Iterate through each provided filename and process it */
+    for (i = 1; i < argc; i++) {
         create_new_symbol_list();
         fileName = argv[i];
-        if ((pre_proccesor = preprocessor(fileName)) == false)
-        {
-            printf("ERROR: pre preprocessor of %s failed.\n", fileName);
+
+        /* Preprocess the file and check for failure */
+        pre_proccesor = preprocessor(fileName);
+        if (!pre_proccesor) {
+            printf("ERROR: Preprocessor of %s failed.\n", fileName);
             free_list();
             removeOutputs(fileName);
             continue;
         }
-        if ((first_pass = firstPass(fileName)) == false)
-        {
-            printf("ERROR: first pass of %s failed.\n", fileName);
+
+        /* Run the first pass on the file and check for failure */
+        first_pass = firstPass(fileName);
+        if (!first_pass) {
+            printf("ERROR: First pass of %s failed.\n", fileName);
             free_list();
             removeOutputs(fileName);
             continue;
         }
-        if ((second_pass = secondPass(fileName)) == false)
-        {
-            printf("ERROR: second pass of %s failed.\n", fileName);
+
+        /* Run the second pass on the file and check for failure */
+        second_pass = secondPass(fileName);
+        if (!second_pass) {
+            printf("ERROR: Second pass of %s failed.\n", fileName);
             free_list();
             removeOutputs(fileName);
             continue;
         }
+
         free_list();
 
+        /* Print the object file */
         printOBJ(fileName);
     }
 
